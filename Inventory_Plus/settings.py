7cdfd2decdea604404c5c32de111180b19e48383
Turki20 <ttaiealmutairi@stu.kau.edu.sa>
2025-08-12 12:52:45 +0300

    finish

--- Inventory_Plus/settings.py
+++ Inventory_Plus/settings.py
@@ -135,12 +135,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# load_dotenv()
+load_dotenv()
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587  
-EMAIL_HOST_USER = 'ttuurr556@gmail.com'# os.getenv('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_HOST_PASSWORD = 'jsbz fhbr kfry scxg'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
+
+print(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
